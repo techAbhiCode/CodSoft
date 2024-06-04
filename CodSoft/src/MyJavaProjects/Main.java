c70e0a2f594e9d6987933c9d244d965ee63aafa0
techAbhiCode <166912573+techAbhiCode@users.noreply.github.com>
2024-06-04 20:10:54 +0530

    Update Main.java

--- CodSoft/src/MyJavaProjects/Main.java
+++ CodSoft/src/MyJavaProjects/Main.java
@@ -78,6 +78,7 @@ class BankAccount{
 public class Main {
     public static void main(String[] args) {
         BankAccount userAccount = new BankAccount(1000);
+         ATM atm = new ATM(userAccount);
 
     }
 }
